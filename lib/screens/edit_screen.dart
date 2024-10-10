f200324e87c4513c9b14c0242457186230154a2b
Kanathip001 <kanathipb65@nu.ac.th>
2024-10-10 23:41:26 +0700

    edit-changetallscreen

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,13 +1,14 @@
 import 'package:account/main.dart';
 import 'package:account/models/transactions.dart';
 import 'package:account/provider/transaction_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class EditScreen extends StatefulWidget {
-  final Transactions statement;
+  Transactions statement;
 
-  const EditScreen({super.key, required this.statement});
+  EditScreen({super.key, required this.statement});
 
   @override
   State<EditScreen> createState() => _EditScreenState();
@@ -15,118 +16,110 @@ class EditScreen extends StatefulWidget {
 
 class _EditScreenState extends State<EditScreen> {
   final formKey = GlobalKey<FormState>();
+
   final nameCtl = TextEditingController();
   final assetCtl = TextEditingController();
   final founderCtl = TextEditingController();
   final placeCtl = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
+  Widget build(BuildContext context) {
     nameCtl.text = widget.statement.bankname;
     placeCtl.text = widget.statement.place;
     founderCtl.text = widget.statement.founder;
     assetCtl.text = widget.statement.asset.toString();
-  }
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('แก้ไขข้อมูล'),
-        backgroundColor: const Color.fromARGB(255, 255, 74, 61),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+        appBar: AppBar(
+          title: const Text('แบบฟอร์มแก้ไขข้อมูล'),
+        ),
+        body: SingleChildScrollView(
           child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                _buildTextField(
-                  label: 'ชื่อธนาคาร',
-                  controller: nameCtl,
-                ),
-                _buildTextField(
-                  label: 'ที่ตั้ง',
-                  controller: placeCtl,
-                ),
-                _buildTextField(
-                  label: 'ผู้ก่อตั้ง',
-                  controller: founderCtl,
-                ),
-                _buildTextField(
-                  label: 'สินทรัพย์',
-                  controller: assetCtl,
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 255, 74, 61), // ปรับสีพื้นหลัง
-                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30), // เพิ่มมุมมน
+              key: formKey,
+              child: Column(
+                children: [
+                    Container(
+                      margin: const EdgeInsets.all(30),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'ชื่อธนาคาร',
+                            labelStyle: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        autofocus: false,
+                        controller: nameCtl,
+                        validator: (String? str) {
+                          if (str!.isEmpty) {
+                            return 'กรุณากรอกข้อมูล';
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  child: const Text(
-                    'แก้ไขข้อมูล',
-                    style: TextStyle(fontSize: 18, color: Colors.white), // ปรับสีข้อความ
-                  ),
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      var statement = Transactions(
-                        keyID: widget.statement.keyID,
-                        bankname: nameCtl.text,
-                        place: placeCtl.text,
-                        founder: founderCtl.text,
-                        asset: double.parse(assetCtl.text),
-                      );
+                    Container(
+                      margin: const EdgeInsets.all(30),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'ที่ตั้ง',
+                            labelStyle: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        autofocus: false,
+                        controller: nameCtl,
+                        validator: (String? str) {
+                          if (str!.isEmpty) {
+                            return 'กรุณากรอกข้อมูล';
+                          }
+                        },
+                      ),
+                    ),
+            
+                    Container(
+                      margin: const EdgeInsets.all(30),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'ยี่ห้อ',
+                            labelStyle: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        controller: founderCtl,
+                      ),
+                    ),
+            
+                    Container(
+                      margin: const EdgeInsets.all(30),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'รุ่น',
+                            labelStyle: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        controller: assetCtl,
+                      ),
+                    ),
+                    
+                  TextButton(
+                      child: const Text('แก้ไขข้อมูล'),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          // create transaction data object
+                          var statement = Transactions(
+                                keyID: widget.statement.keyID,
+                                bankname: nameCtl.text,
+                                place: placeCtl.text,
+                                founder: founderCtl.text,
+                                asset:double.parse(assetCtl.text),
+                                );
 
-                      var provider = Provider.of<TransactionProvider>(context, listen: false);
-                      provider.updateTransaction(statement);
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const MyHomePage(),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTextField({
-    required String label,
-    required TextEditingController controller,
-    TextInputType? keyboardType,
-  }) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: TextFormField(
-        decoration: InputDecoration(
-          labelText: label,
-          labelStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15), // ทำให้ขอบมน
-          ),
-          contentPadding: const EdgeInsets.all(10),
-        ),
-        controller: controller,
-        keyboardType: keyboardType,
-        validator: (String? str) {
-          if (str!.isEmpty) {
-            return 'กรุณากรอกข้อมูล';
-          }
-          return null;
-        },
-      ),
-    );
+                          // add transaction data object to provider
+                          var provider = Provider.of<TransactionProvider>(context, listen: false);
+                          provider.updateTransaction(statement);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  fullscreenDialog: true,
+                                  builder: (context) {
+                                    return MyHomePage();
+                                  }));
+                        }
+                      })
+                ],
+              )),
+        ));
   }
 }
